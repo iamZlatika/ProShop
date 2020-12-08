be1dce8c56fa9b7de506604933ee508fb4836abe
iamzlatika <mazurovazv@gmail.com>
2020-12-08 17:52:46 +0300

    Add Spinner and Error message with react-bootstrap

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
 import {listProducts} from '../actions/productActions'
 import { productListReducer } from '../redusers/productReducers'
 
@@ -22,7 +24,7 @@ const HomeScreen = () => {
   return (
     <>
       <h1>Latest Products</h1>
-  {loading ? <h1>Loading</h1> : error ? <h3>{error}</h3>:
+  {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message>:
         <Row>
         {products.map(product => (
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
