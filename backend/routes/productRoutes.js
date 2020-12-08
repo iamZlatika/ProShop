e4ed8428a151acc4ab1bb72efbb91d4cd760f691
iamzlatika <mazurovazv@gmail.com>
2020-12-08 17:29:41 +0300

    Bring Redux&State into Home Screen

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,14 +6,18 @@ import Product from '../models/productModel.js'
 
 //  Fetch all products - GET/api/products - public access
 router.get('/', asyncHandler(async (req, res) => {
-  const products = await Product.find({})
+    const products = await Product.find({})
+    // res.status(401)
+    // throw new Error('Not Authorized')
   res.json(products)
 }))
 
 
 //  Fetch a product - GET/api/products/:id - public access
 router.get('/:id', asyncHandler(async (req, res) => {
+
   const product = await Product.findById(req.params.id)
+
   if (product) {
     res.json(product)
   } else {
